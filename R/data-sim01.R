61268feebd3cf989faf6b6838c46e7b012aa5030
Mark Jones <mark.jones1@sydney.edu.au>
2025-09-09 10:02:09 +0800

    Factor out enrol time

--- R/data-sim01.R
+++ R/data-sim01.R
@@ -3,13 +3,6 @@ library(data.table)
 
 
 
-get_sim01_enrol_time <- function(N = 2500, lambda = 1.52,
-                               rho = function(t) pmin(t/360, 1)){
-  
-  c(0, poisson::nhpp.event.times(lambda, N - 1, rho))
-}
-
-
 
 get_sim01_trial_data <- function(
     l_spec
